9edff8ebe8d764174d5e8143e8c01d241a02f88d
Joann Saeou <joannssaeou@gmail.com>
2022-11-02 21:46:07 -0500

    deploy

---
+++ ifelse/src/main/java/com/joann/ifelse/ifelse.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joann.ifelse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author joann
+ */
+public class ifelse {
+    public static void main(String[] args){
+    
+        if(20 > 18){
+            System.out.println("The number is greater than 18");
+            
+           int time = 20;
+          if(time<18) {
+              System.out.println("Good day.");
+          } else {
+          System.out.println("Good evening!");
+          }
+           
+          
+          
+          int hours = 20;
+          String result = (hours < 18) ? "Good day" : "Good evening";
+          System.out.println(result);
+   
+          
+          
+int sum = 100;
+int num2 = 500;
+float weight = 2.5f;
+          
+
+
+if( sum == num2){
+System.out.println("false");
+} else if(sum < num2) {
+System.out.println("false");
+} else if(sum < num2) {
+    System.out.println("True");
+}
+
+else {
+System.out.println("Unavailable");
+}
+          
+          
+        
+        
+        
+        }
+    }
+    
+}
